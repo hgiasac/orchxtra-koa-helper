4bd9c2037faf6aa3b3af0a31d30865a1e3002f87
Toan Nguyen <toannp90@gmail.com>
2018-05-02 12:36:20 +0800

    update logger DBContext

--- src/handler.ts
+++ src/handler.ts
@@ -1,6 +1,11 @@
 import * as Knex from "knex";
 import { Context } from "koa";
 
+export interface ILogger {
+  log: (tag: string, payload: any) => void;
+}
+
 export interface IDBContext extends Context {
   db: Knex;
+  logger: ILogger;
 }
