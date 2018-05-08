39d6203fd12b3e7b3d1a3e0a5e81c5a7579856a8
Toan Nguyen <toannp90@gmail.com>
2018-05-08 11:10:32 +0800

    update auth middleware option

--- src/authorize.ts
+++ src/authorize.ts
@@ -43,29 +43,37 @@ export interface IAuthContext extends IDBContext {
   authUser: IAuthUser;
 }
 
-export function getProfileByAuthId(authId: string): AxiosPromise<IProfile> {
+export function getProfileByAuthId(authId: string, options?: {
+  baseURL?: string
+}): AxiosPromise<IProfile> {
+  const { baseURL = process.env.AUTH_API_HOST } = { ...options };
+
   return axios.get(`/users/oauth/${authId}`, {
-    baseURL: process.env.AUTH_API_HOST
+    baseURL
   });
 }
 
 export interface IAuthMiddlewareOptions {
+  authBaseURL?: string;
   authIdHeaderName?: string;
 }
 
 export function AuthMiddleware(options?: IAuthMiddlewareOptions) {
 
-  const { authHeadeauthIdHeaderNamer = COGNITO_USERNAME_HEADER } = { ...options };
+  const {
+    authIdHeaderName = COGNITO_USERNAME_HEADER,
+    baseURL = process.env.AUTH_API_HOST,
+  } = { ...options };
 
   return async (ctx: IAuthContext, next: () => any) => {
 
-    const username = ctx.headers[authHeadeauthIdHeaderNamer];
+    const username = ctx.headers[authIdHeaderName];
     if (!username) {
       ctx.throw(404);
     }
 
     try {
-      const userResult = await getProfileByAuthId(username);
+      const userResult = await getProfileByAuthId(username, { baseURL });
       const profile: IProfile = userResult.data;
       ctx.authUser = profile;
       next();
