6298064ffa4dbab95def2d52750c421a264804d7
Toan Nguyen <toannp90@gmail.com>
2018-06-04 16:43:48 +0700

    update findServiceAccountModel

--- src/handler.ts
+++ src/handler.ts
@@ -26,3 +26,31 @@ export interface IAuthenticatedHeader {
   "x-orchxtra-display-name": string;
   "x-orchxtra-email": string;
 }
+
+export async function findServiceAccountModel(
+  ctx: IDBContext,
+  builder: (knex: Knex) => Knex.QueryBuilder,
+  id: string,
+  required = false
+) {
+
+  const headers: IAuthenticatedHeader = ctx.headers;
+  const serviceAccountId = headers["x-orchxtra-service-account-id"];
+  const model = await builder(ctx.db)
+    .select("*")
+    .where("id", id)
+    .limit(1);
+
+  if (!model || !model.length) {
+    if (!required) {
+      return null;
+    }
+    ctx.throw(400, `Can not find model by ID: ${id}`);
+  }
+
+  if (model.serviceAccountId !== serviceAccountId) {
+    ctx.throw(403);
+  }
+
+  return model[0];
+}
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,5 @@ export * from "./util";
 export * from "./handler";
 export * from "./healthCheck";
 export * from "./logger";
-export * from "./authorize";
 export * from "./test";
 export * from "./error";
