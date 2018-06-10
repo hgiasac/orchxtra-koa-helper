f2c03b28d1d7a6dcde8954f9d63193a2b093d5d4
Toan Nguyen <toannp90@gmail.com>
2018-06-11 01:29:52 +0700

    update base64 encode and decode function

--- src/util.ts
+++ src/util.ts
@@ -96,3 +96,28 @@ export function deltaTime(start: number): string {
     ? delta + "ms"
     : Math.round(delta / 1000) + "s");
 }
+
+export interface IFileInput {
+  data: Buffer;
+  extension: string;
+}
+
+export function decodeBase64(input: string, fileType = "image"): IFileInput {
+  const data = input.replace(/^data:/, "");
+  const regex = new RegExp(`${fileType}\/[^;]+`);
+
+  const type = data.match(regex);
+  const base64 = data.replace(/^[^,]+,/, "");
+  const extension = type ? type[0] : undefined;
+
+  return {
+    extension,
+    data: Buffer.from(base64, "base64"),
+  };
+}
+
+export function encodeBase64(input: ArrayBuffer, extension: string): string {
+  const metadata = extension ? `data:${extension};base64,` : "";
+
+  return metadata + Buffer.from(input).toString("base64");
+}
